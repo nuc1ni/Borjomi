98b257f09698583322458661f2e71425931fb340
nuc1ni <ninikordzaia@gmail.com>
2023-07-26 02:37:34 +0400

    changes

---
+++ css/script.js
@@ -0,0 +1,7 @@
+let nav = document.getElementById('navBar');
+let burger = document.getElementById('burgerBar');
+
+
+burger.addEventListener('click', function () {
+navBar.classlist.add('activeNav');
+} )
