21a3c9d197884e304ca5af4aaa8db1da78b031e7
nuc1ni <ninikordzaia@gmail.com>
2023-07-26 20:43:46 +0400

    media320-480

--- css/script.js
+++ css/script.js
@@ -1,7 +1,6 @@
-let nav = document.getElementById('navBar');
-let burger = document.getElementById('burgerBar');
+let navBar =document.getElementById("navBar");
+let burgerBar = document.getElementById("burgerBar");
 
-
-burger.addEventListener('click', function () {
-navBar.classlist.add('activeNav');
-} )
+burgerBar.addEventListener('click', function() {
+    navBar.classList.toggle("activeNav");
+});
